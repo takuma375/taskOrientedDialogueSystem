f9ec8269069ec92a4f03c753382522d93c1b7614
takuma375 <takuma-shimizu-10174@keio.jp>
2021-07-21 23:31:54 +0900

    Update 23行目のsentの部分のindexに誤りがあったため修正
    その他にも、文法の微修正を行った

--- frameBased/crf_util.py
+++ frameBased/crf_util.py
@@ -18,12 +18,12 @@ def word2features(sent, i):
     else:
         features['BOS'] = True
     
-    if i < len(sent) - 1 :
+    if i < len(sent)-1:
         word_right = sent[i+1][0]
-        postag_right = sent[i+1][0]
+        postag_right = sent[i+1][1]
         features.update({
             '+1:word': word_right,
-            "+1:postag": postag_right
+            '+1:postag': postag_right
         })
     else:
         features['EOS'] = True
